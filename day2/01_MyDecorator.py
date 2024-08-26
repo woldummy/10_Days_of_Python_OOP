7f74bfc971d0d0e591ea9ed73bd9354cd727f3b3
woldummy <woldummy@web.de>
2024-08-26 17:54:48 +0200

    day32: added comments to MyDecorator

--- day2/01_MyDecorator.py
+++ day2/01_MyDecorator.py
@@ -1,6 +1,10 @@
 # -*- coding: UTF-8 -*-
 # Ten days of Python Day 2
 # showing how to use a decorator without the @ syntax
+# functions are first-class objects in Python
+# To call an innerFunction(), we must first call outerFunction().
+# The outerFunction() will then go ahead and call innerFunction() as it has
+# been defined inside it.
 # Name: wolke
 # Date: 2024-08-13
 # macOS: 14.2.1  Python: 3.12
@@ -19,5 +23,7 @@ def say_hello():
 
 if __name__ == "__main__":
     # The function is wrapped by the decorator
+    print(say_hello.__name__)
     say_hello = my_decorator(say_hello)  # say_hello now points to inner wrapper function
     say_hello()
+    print(say_hello.__name__)
